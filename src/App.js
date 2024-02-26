c153173a22fb692ef611b95e026a21e249df409b
mnrx2020 <jamesmbere@gmail.com>
2024-02-27 02:14:43 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MemeHeader from "./npages/MemeHeader";
+import MemeComponent from "./npages/MemeComponent";
+import Footer from "./npages/Footer"
+
+
+export default function App(){
+  return (
+    <div>
+      <MemeHeader />
+      <MemeComponent />
+      <Footer />
+    </div>
+  )
+  }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-import reportWebVitals from './reportWebVitals';
->>>>>>> eba9e304f28506d4b4be8daaef8ef0b46b85af17
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,10 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
->>>>>>> eba9e304f28506d4b4be8daaef8ef0b46b85af17
